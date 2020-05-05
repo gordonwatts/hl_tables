5973e144337da3011ca2b55c4952a7753d308848
Gordon Watts <gwatts@uw.edu>
2020-05-05 01:46:05 -0700

    Get DF production sort-of working

--- hl_tables/atlas/vectors.py
+++ hl_tables/atlas/vectors.py
@@ -1,11 +1,17 @@
+from __future__ import annotations
+import ast
 
 from dataframe_expressions import DataFrame
 
 
-class _atlas_3v:
+class _atlas_3v_ast(ast.AST):
     '3Vector from an atlas dataframe'
-    def __init__(self, df: DataFrame):
-        self._df = df
+    def __init__(self, seq: ast.AST):
+        '''
+        Create an ast that represents a 3-Vector
+        '''
+        self.base_seq = seq
+        self._fields = ('base_seq', )
 
 
 def a_3v(df: DataFrame):
@@ -13,4 +19,4 @@ def a_3v(df: DataFrame):
     Return a 3-vector object from the current data frame. The x,y, and z components
     are assumed to be in the (df.x, df.y, df.z) components.
     '''
-    return _atlas_3v(df)
+    return DataFrame(df, expr=_atlas_3v_ast(ast.Name(id='p')))
--- tests/atlas/test_vectors.py
+++ tests/atlas/test_vectors.py
@@ -6,3 +6,12 @@ def test_3v_create():
     df = DataFrame()
     v = a_3v(df)
     assert v is not None
+
+
+def test_3v_subtract():
+    df = DataFrame()
+    v1 = a_3v(df)
+    v2 = a_3v(df)
+    r = v1 - v2
+    assert r is not None
+    assert isinstance(r, DataFrame)
