271d687d6ea6cdaba50255b8126e2f89aed43c0b
Gordon Watts <gwatts@uw.edu>
2020-05-07 17:05:46 -0700

    Detect where we can't get at this.

--- hl_tables/servicex/xaod_runner.py
+++ hl_tables/servicex/xaod_runner.py
@@ -1,7 +1,7 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Optional
 import ast
 
-from dataframe_expressions import DataFrame, ast_DataFrame
+from dataframe_expressions import DataFrame, ast_DataFrame, Column
 
 from ..runner import result, runner, ast_awkward
 import hep_tables
@@ -10,7 +10,24 @@ import hep_tables
 #       in the `hep_tables` package.
 
 
-def _can_process(a: ast.AST) -> bool:
+def has_df_ref(a: ast.AST):
+    class find_it(ast.NodeVisitor):
+        def __init__(self):
+            self.seen = False
+
+        def visit_ast_DataFrame(self, node: ast_DataFrame):
+            self.seen = True
+
+        def visit_Name(self, node: ast.Name):
+            if node.id == 'p':
+                self.seen = True
+
+    f = find_it()
+    f.visit(a)
+    return f.seen
+
+
+def _can_process(a: Optional[ast.AST]) -> bool:
     'Is this an AST we can deal with?'
 
     # Does the ast contain a binary operation, where each side refers to an iterator?
@@ -20,18 +37,28 @@ def _can_process(a: ast.AST) -> bool:
         def __init__(self):
             self.bad_binary = False
 
-        def visit_BinOp(self, node: ast.BinOp):
-            if _can_process(node.left) and _can_process(node.right):
+        def check_args(self, asts):
+            count = sum([1 for a in asts if has_df_ref(a)])
+            if count > 1:
                 self.bad_binary = True
-            else:
-                self.generic_visit(node)
+
+        def visit_BinOp(self, node: ast.BinOp):
+            self.generic_visit(node)
+            self.check_args([node.left, node.right])
+
+        def visit_Compare(self, node: ast.Compare):
+            self.generic_visit(node)
+            self.check_args([node.left] + node.comparators)
+
+    if a is None:
+        return True
 
     d = detect_binary()
     d.visit(a)
     return not d.bad_binary
 
 
-def _process_ast(a: ast.AST, parent: DataFrame) -> ast.AST:
+def _process_ast(a: Optional[ast.AST], parent: DataFrame) -> Optional[ast.AST]:
     'Process everything we can in the ast'
 
     class process(ast.NodeTransformer):
@@ -50,6 +77,9 @@ def _process_ast(a: ast.AST, parent: DataFrame) -> ast.AST:
         def visit_ast_DataFrame(self, a: ast_DataFrame):
             return self.make_local_df(a.dataframe)
 
+    if a is None:
+        return None
+
     return process(parent).visit(a)
 
 
@@ -57,29 +87,41 @@ class xaod_runner(runner):
     '''
     We can do a xaod on servicex
     '''
-    def _process_by_depth(self, df: DataFrame) -> Tuple[bool, Union[DataFrame, result]]:
+    def _process_by_depth(self, df: Union[DataFrame, Column]) \
+            -> Tuple[bool, Union[DataFrame, Column, result]]:
         'Process by depth'
 
         # Lets do the really easy thing first - what happens when we are at the bottom.
-        if df.parent is None:
-            assert df.child_expr is None, 'Internal Programming Error'
-            return (isinstance(df, hep_tables.xaod_table), df)
+        if isinstance(df, DataFrame):
+            if df.parent is None:
+                assert df.child_expr is None, 'Internal Programming Error'
+                return (isinstance(df, hep_tables.xaod_table), df)
 
-        # Lets see if the parent can be processed. If not, then we just continue
-        # to return false.
-        can_process, modified_df = self._process_by_depth(df.parent)
-        if not can_process:
-            return (can_process, modified_df)
+            # Lets see if the parent can be processed. If not, then we just continue
+            # to return false.
+            can_process, modified_df = self._process_by_depth(df.parent)
+            if not can_process:
+                df.parent = modified_df
+                return (can_process, df)
 
-        # Parent can be processed. This is great. Lets check if the child expression
-        # can be processed.
-        if not _can_process(df.child_expr):
-            # Process everything in the expression we can
-            new_expr = _process_ast(df.child_expr, df.pare)
-            df.child_expr = new_expr
-            return (False, df)
+            # Parent can be processed. This is great. Lets check if the child expression
+            # can be processed.
+            if not _can_process(df.child_expr) or not _can_process(df.filter):
+                # Process everything in the expression we can
+                new_child = _process_ast(df.child_expr, df.parent)
+                new_filter = _process_ast(df.filter, df.parent)
+                df.child_expr = new_child
+                df.filter = new_filter
+                return (False, df)
 
-        return (True, df)
+            return (True, df)
+        elif isinstance(df, Column):
+            if not _can_process(df.child_expr):
+                df.child_expr = _process_ast(df.child_expr, None)
+                return (False, df)
+            return (True, df)
+        else:
+            raise Exception('Internal programing error')
 
     def process(self, df: DataFrame) -> Union[DataFrame, result]:
         'Process as much of the tree as we can process'
--- tests/servicex/test_xaod_runner.py
+++ tests/servicex/test_xaod_runner.py
@@ -1,9 +1,9 @@
 import ast
 
-from dataframe_expressions import DataFrame
+from dataframe_expressions import DataFrame, Column
 from func_adl import EventDataset
 from hep_tables import xaod_table
-from hl_tables.runner import result, runner
+from hl_tables.runner import ast_awkward, result, runner
 from hl_tables.servicex.xaod_runner import xaod_runner
 import pytest
 
@@ -36,7 +36,19 @@ def test_with_result(good_xaod, hep_tables_make_local_call):
     hep_tables_make_local_call.assert_called_once_with(df1)
 
 
-def test_split_call(good_xaod, hep_tables_make_local_call):
+def test_with_filter_result(good_xaod, hep_tables_make_local_call):
+    # Hand it something it can do, and then see if it can make it work!
+    x = xaod_runner()
+    df1 = good_xaod[good_xaod.x > 10].x
+    r = x.process(df1)
+
+    assert r is not None
+    assert isinstance(r, result)
+    assert r.result is not None
+    hep_tables_make_local_call.assert_called_once_with(df1)
+
+
+def test_split_call_binary(good_xaod, hep_tables_make_local_call):
     x = xaod_runner()
     df1 = good_xaod.x + good_xaod.y
     r = x.process(df1)
@@ -45,3 +57,57 @@ def test_split_call(good_xaod, hep_tables_make_local_call):
     assert hep_tables_make_local_call.call_count == 2
     a = r.child_expr
     assert isinstance(a, ast.BinOp)
+    assert isinstance(a.left, ast_awkward)
+    assert isinstance(a.right, ast_awkward)
+
+
+def test_split_call_compare(good_xaod, hep_tables_make_local_call):
+    x = xaod_runner()
+    df1 = good_xaod.x > good_xaod.y
+    r = x.process(df1)
+    assert r is not None
+    assert isinstance(r, Column)
+    assert hep_tables_make_local_call.call_count == 2
+    a = r.child_expr
+    assert isinstance(a, ast.Compare)
+    assert isinstance(a.left, ast_awkward)
+    assert isinstance(a.comparators[0], ast_awkward)
+
+
+def test_split_call_good_filter(good_xaod, hep_tables_make_local_call):
+    x = xaod_runner()
+    ok_events = good_xaod[good_xaod.x > 10]
+    df1 = ok_events.x + ok_events.y
+    r = x.process(df1)
+    assert r is not None
+    assert isinstance(r, DataFrame)
+    assert hep_tables_make_local_call.call_count == 2
+    a = r.child_expr
+    assert isinstance(a, ast.BinOp)
+    assert isinstance(a.left, ast_awkward)
+    assert isinstance(a.right, ast_awkward)
+
+
+def test_split_in_filter(good_xaod, hep_tables_make_local_call):
+    x = xaod_runner()
+    ok_events = good_xaod[good_xaod.x > good_xaod.y]
+    df1 = ok_events.z
+    r = x.process(df1)
+    assert r is not None
+    assert isinstance(r, DataFrame)
+    assert hep_tables_make_local_call.call_count == 3
+    a = r.child_expr
+    assert isinstance(a, ast.BinOp)
+    assert isinstance(a.left, ast_awkward)
+    assert isinstance(a.right, ast_awkward)
+
+
+def test_split_in_sqrt(good_xaod, hep_tables_make_local_call):
+    x = xaod_runner()
+    import numpy as np
+    df1 = np.sqrt(good_xaod.x + good_xaod.y)
+    r = x.process(df1)
+    assert r is not None
+    assert isinstance(r, DataFrame)
+    assert hep_tables_make_local_call.call_count == 2
+    assert isinstance(r.child_expr, ast.Call)
